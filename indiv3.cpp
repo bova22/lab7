b68bc6a73d96dca115de7c2d49dc279089d13c1b
bova22 <olegobovo@gmail.com>
2025-01-17 08:25:29 +0300

    Update indiv3.cpp

--- indiv3.cpp
+++ indiv3.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-const double PI = 3.141592653589793;  // Явное указание значения PI
+const double PI = 3.141592653589793;  
 long double factorial(int с) {
     if (с == 0 || с == 1) return 1.0;
     long double result = 1.0;
@@ -14,19 +14,18 @@ long double factorial(int с) {
     return result;
 }
 
-// Разложение функции в ряд
 double S(int n, double x) {
-    double sum = 1.0;   // Начальный член ряда
-    double term = 1.0;  // Инициализация первого члена ряда
-
+    double sum = 1.0;   
+    double term = 1.0;  
+    
     for (int i = 1; i <= n; ++i) {
-        term *= (x * cos(PI / 4)) / factorial(i);  // Величина каждого члена ряда
+        term *= (x * cos(PI / 4)) / factorial(i); 
         sum += term;  // Добавляем к сумме
     }
     return sum;
 }
 
-// Функция Y, которую нужно сравнить с разложением
+// Функция Y
 double Y(double x) {
     return exp(x * cos(PI / 4)) * cos(x * sin(PI / 4));
 }
@@ -35,14 +34,13 @@ int main() {
     setlocale(LC_ALL, "Rus");
 
     const double a = 0.1, b = 1;
-    const int n = 120;  // Число членов ряда
-    int steps = 10;  // Количество шагов
+    const int n = 120;  
+    int steps = 10; 
     double h = (b - a) / steps;  // Шаг по x
 
     cout << "x: \t \t | S(x) \t \t | Y(x) \n";
     cout << "-----------------------------\n";
 
-    // Вычисление значений и вывод
     for (int i = 0; i <= steps; ++i) {
         double x = a + i * h;
         cout << fixed << setprecision(6) << x << "\t | "
